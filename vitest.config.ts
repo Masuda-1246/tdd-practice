4323fcfff2f41cbdadde2748bf4a71db793c2a0a
Masuda-1246 <yaojiezengtian38@gmail.com>
2025-05-25 10:36:46 +0900

    初期化

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import path from 'node:path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules/', 'dist/'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@domain': path.resolve(__dirname, './src/domain'),
+      '@application': path.resolve(__dirname, './src/application'),
+      '@infrastructure': path.resolve(__dirname, './src/infrastructure'),
+      '@presentation': path.resolve(__dirname, './src/presentation'),
+    },
+  },
+});
